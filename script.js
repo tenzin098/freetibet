c3bdac56d2872150dea455b409f279cddb48bf74
Tenzin Yangchen <yangchent723@gmail.com>
2025-07-04 11:11:56 -0700

    Create script.js
    Signed-off-by: Tenzin Yangchen <yangchent723@gmail.com>

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+      // This function runs when the button is clicked
+      function greetUser() {
+        let name = prompt("What's your name? \n ཁྱེད་རང་གི་མིང་ལ་ག་རེ་རེད།");
+        if (name) {
+          alert("Tashi Delek བཀྲ་ཤིས་བདེ་ལེགས།, " + name + "! 👋");
+        } else {
+          alert("You didn't enter a name.");
+        }
+      }
+    </script>
